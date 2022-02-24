498d2985b9b43e62542d024f89f9383a80f95030
Domagoj <domagoj.majcenic@9y.co>
2022-02-24 15:49:04 +0100

    Implement unit tests for Mail service

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -5,6 +5,8 @@ import { ConfigService } from '@nestjs/config';
 
 describe('MailService', () => {
   let service: MailService;
+  let mailerServiceMock: MailerService;
+  let configServiceMock: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,19 +14,50 @@ describe('MailService', () => {
         MailService,
         {
           provide: MailerService,
-          useValue: {},
+          useValue: {
+            sendMail: jest.fn(),
+          },
         },
         {
           provide: ConfigService,
-          useValue: {},
+          useValue: {
+            get: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<MailService>(MailService);
+    mailerServiceMock = module.get<MailerService>(MailerService);
+    configServiceMock = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sendMail', () => {
+    it("should call the mailer service's sendMail method and return it's return value", async () => {
+      const sendMailOptions = {
+        html: '<h1>hello<h1>',
+        subject: 'An email subject',
+        text: 'hello',
+        to: 'test@mail.com',
+      };
+
+      const returnValue = { info: 'test' };
+
+      jest.spyOn(configServiceMock, 'get').mockReturnValue('test');
+      jest.spyOn(mailerServiceMock, 'sendMail').mockResolvedValue(returnValue);
+
+      expect(await service.sendMail(sendMailOptions)).toStrictEqual(
+        returnValue,
+      );
+
+      expect(mailerServiceMock.sendMail).toHaveBeenCalledWith({
+        from: expect.any(String),
+        ...sendMailOptions,
+      });
+    });
   });
 });
